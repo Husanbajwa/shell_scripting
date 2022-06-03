0a6069c1fac2041ecf593f1a123d0a74af73dcd3
Ubuntu <ubuntu@ip-172-31-20-239.ec2.internal>
2022-06-03 18:49:51 +0000

    modify disk check file and add a cronjob to run disk check file every day t 10:00

--- 2-disk_check.sh
+++ 2-disk_check.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-alert=45
+alert=40
+current_time=$(date "+%d-%h-%Y")
 df -H | awk '{print $5" " $1}'| while read output;
 do
  usage=$(echo $output | awk '{print $1}' | cut -d'%' -f1)
  file_sys=$(echo $output | awk '{print $2}')
  if [ $usage -ge $alert ]
  then
- echo "critical for $file_sys"
+ echo "CRITICAL for $file_sys  ($usage %)   AT $current_time"
  fi
 done
